042ce235995fbfddbc788003bebc1d6b98df9bc0
EICTAdmin <144975114+EICTAdmin@users.noreply.github.com>
2025-07-04 14:09:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,6 @@
 
 cd /rustdesk-api-server;
 
-if [ ! -e "./db/db.sqlite3" ]; then
-    cp "./db_bak/db.sqlite3" "./db/db.sqlite3"
-    echo "No database present, so copied initial database"
-fi
-
 echo "Making migrations.."
 python manage.py makemigrations
 echo "Migrating.."
